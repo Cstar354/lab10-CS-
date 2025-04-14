1cc08e66028cadc18ce2302e42593fb2e8ff43ea
Cstar354 <cstar354@gmail.com>
2025-04-14 02:37:40 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,7 +1,7 @@
-# https://github.com/Cstar354/lab10-CS-
+# https://github.com/Cstar354/lab10-CS-CS
 # Partner 1: Camari
 # Partner 2: William
-# git clone https://github.com/Cstar354/lab10-CS-
+# git clone https://github.com/Cstar354/lab10-CS-CS
 # cd lab10-CS-
 # git add calculator.py
 # git commit -m "modified calculator p1"
