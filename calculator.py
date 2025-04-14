2e4b40fb1029426bd13a688697754fddf1f33e26
Cstar354 <cstar354@gmail.com>
2025-04-14 00:55:19 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,11 +1,38 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
-
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    pass
+git clone https://github.com/Cstar354/lab10-CS-
+cd https://github.com/Cstar354/lab10-CS-
+git add calculator.py
+git committ - m "modified calculator p1"
+git push
 
 
+import math
+def square_root(a):
+    try: 
+        a < 0:
+            raise ValueError
+        return math.sqrt(a)
+    except ValueError as e:
+        raise e
+def hyptenuse(a,b):
+    math.hypot(a,b)
+
+def add(a,b): 
+    return a + b
+def subtract(a,b):
+    return a - b
+def multiply(a,b):
+    return a * b
+def divide(a,b):
+     if a==0 
+        raise ZeroDivisionError
+    return a/b
+def logarithm(a,b):
+    if a<=0 or a==1
+        raise ValueError
+    if b <= 0
+        raise ValueError
+    return math.log(b,a)
+
+def exponent(a,b):
+    return a**b
+    
