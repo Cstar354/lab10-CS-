5416cafb6136e354f3b94c706f7982b675ffe853
Cstar354 <cstar354@gmail.com>
2025-04-14 02:14:05 -0400

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,13 +1,3 @@
-# https://github.com/Cstar354/lab10-CS-
-# Partner 1: Camari 
-# Partner 2: William
-import unittest
-from calculator import *
-
-# git add calculator.py    
-# git commit -m "merged changes"
-# git push	
-
 class TestCalculator(unittest.TestCase):
     ######### Partner 2
     def test_add(self): # 3 assertions
@@ -21,41 +11,41 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(subtract(11, 4), 7)
 
     ######## Partner 1
-    def test_multiply(self): # 3 assertions
-        self.assertEqual(multiply(3, 4), 12)
-        self.assertEqual(multiply(5, 3), 15)
-        self.assertEqual(multiply(2, 1), 2)
+    def test_mul(self): # 3 assertions
+        self.assertEqual(mul(3, 4), 12)
+        self.assertEqual(mul(5, 3), 15)
+        self.assertEqual(mul(2, 1), 2)
 
-    def test_divide(self): # 3 assertions
-        self.assertEqual(divide(10, 2), 5.0)
-        self.assertEqual(divide(36, 6), 6.0)
+    def test_div(self): # 3 assertions
+        self.assertEqual(div(10, 2), 5.0)
+        self.assertEqual(div(6, 36), 0.16666666666666666)
         with self.assertRaises(ZeroDivisionError):
-            divide(0, 5)
+            div(0, 5)
 
     ######## Partner 2
-    def test_divide_by_zero(self): # 1 assertion
+    def test_div_by_zero(self): # 1 assertion
         with self.assertRaises(ZeroDivisionError):
             div(0, 5)
 
     def test_logarithm(self): # 3 assertions
-        self.assertAlmostEqual(logarithm(math.e, 1), 0)
-        self.assertAlmostEqual(logarithm(1000, 10), 3)
-        self.assertAlmostEqual(logarithm(8, 2), 3)
+        self.assertAlmostEqual(logarithm(10, 1000), 3)
+        self.assertAlmostEqual(logarithm(2, 8), 3)
+        self.assertAlmostEqual(logarithm(2, 32), 5)
 
     def test_log_invalid_base(self): # 1 assertion
         with self.assertRaises(ValueError):
-            logarithm(10, 1)
+            logarithm(1, 10)
 
-    def test_log_invalid_argument(self): # 1 assertion
+    def test_log_invalid_argument(self):
         with self.assertRaises(ValueError):
-            logarithm(0, 5)
+            logarithm(10, -5)
 
-    def test_hypotenuse(self): # 3 assertions
+    def test_hypotenuse(self):
         self.assertAlmostEqual(hypotenuse(3, 4), 5.0)
-        self.assertAlmostEqual(hypotenuse(6, 10), 9.0)
+        self.assertAlmostEqual(hypotenuse(6, 8), 10.0)
         self.assertAlmostEqual(hypotenuse(0, 0), 0.0)
 
-    def test_sqrt(self): # 3 assertions
+    def test_sqrt(self):
         with self.assertRaises(ValueError):
             square_root(-9)
         self.assertAlmostEqual(square_root(16), 4.0)
